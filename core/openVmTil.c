88e1de5754aa4f7257a132c4b975d9f5c3f1e4b3
Dennis Janssen <integralcreativity@yahoo.com>
2015-09-17 11:15:51 -0700

    added ReadMe.txt file

--- core/openVmTil.c
+++ core/openVmTil.c
@@ -1,6 +1,6 @@
 
 #include "../includes/cfrtil.h"
-#define VERSION ((byte*) "0.762.054" ) 
+#define VERSION ((byte*) "0.762.056" ) 
 
 // the only major extern variable but there are two global structures in primitives.c
 OpenVmTil * _Q_ ;
